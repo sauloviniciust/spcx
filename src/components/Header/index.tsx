262f21a1a13ffecbb6a905fb6f16e48e6c9717b6
Sauloviniciust <sauloviniciust@gmail.com>
2023-10-30 13:15:29 -0300

    editando

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,16 @@
-
-
 const Header = () => {
   return (
-    <header className="bg-[url('saceback.png')] bg-cover bg-center">
+    <header
+      style={{ backgroundImage: "url('/saceback.png')" }}
+      className="bg-cover bg-center"
+    >
       <div className="relative hidden mb-20 md:block">
         <div>
-          <img className="animate-pulse transform scale-50 hover:scale-75"
-            src="logo personalizado sf black.png"
-            alt="spacechronix Logo"/>
+          <img
+            className="animate-pulse transform scale-50 hover:scale-75"
+            src="/logo personalizado sf black.png"
+            alt="spacechronix Logo"
+          />
         </div>
       </div>
     </header>
