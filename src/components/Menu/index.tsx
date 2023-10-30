9d991434f2cfb4e5ace5cf5494a9b90370a80729
Sauloviniciust <sauloviniciust@gmail.com>
2023-10-30 13:53:28 -0300

    editando

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -24,18 +24,19 @@ const Menu = () => {
         <div className={`w-full fixed top-0 z-20 
          transition duration-500 ease-in-out ${
             isScrolled ? "bg-black" : "bg-transparent" }`}>
-<div className="mt-3 mr-10 flex items-center font-sans ">
-   <div className="flex-grow mt-3 hidden w-full md:block md:w-auto">
+<div className="p-3 flex lg:flex-row lg:justify-between md:flex-col items-center font-sans">
+   <div className="hidden w-full md:block md:w-auto">
        <a href="#">
-           <img className="mb-6 hover:animate-pulse " src="spacelogosite3.png"
+           <img className="hover:animate-pulse" src="spacelogosite3.png"
                alt="Spacechronix Logo"  /></a>
    </div>
    <div className="hidden w-full md:block md:w-auto">
-       <ul className="flex space-x-16">
+       <ul className="flex space-x-8">
            <li><a className="text-2xl  hover:text-red-600 " href=".#">Home</a></li>
            <li><a className="text-2xl  hover:text-red-600 " href="#caminhada">Historia</a></li>
            <li><a className="text-2xl  hover:text-red-600 " href=".#conheça">Conheça</a></li>
            <li><a className="text-2xl  hover:text-red-600 " href="#contato">Contato</a></li>
+           <li><a className="text-2xl  hover:text-red-600 " href="https://drive.google.com/drive/folders/1PXMsfCpkuUUQt9ajB1fjrV_zANqjL6km?usp=sharing" target="blank">Presskit</a></li>
        </ul>
    </div>
 </div>
